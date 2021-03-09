061e4ffbcc83b7a87e2c4b26433578fc1f5a1e4b
fernabe <fer.barrabes@gmail.com>
2021-03-09 09:09:47 +0100

    Change splash img

--- screens/PortfolioItem.js
+++ screens/PortfolioItem.js
@@ -1,52 +1,50 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const dimensions = Dimensions.get('window');
-const imageHeight = Math.round(dimensions.width * 9 / 16);
 const imageWidth = dimensions.width;
 
 
 const PortfolioItem = ({navigation, title, image, content}) => {  
     return(
-        <View style={styles.item}>
-            <Image 
-            source={{ uri: image}}
-            style={styles.image}
-            resizeMode='cover'
-            />
-            <Text 
-                style={styles.title} 
-                onPress={() =>
-                    navigation.navigate('Profile', { 
-                        title,
-                        content,
-                        image
-                    }   
-                )}
-                >
-                {title}
-            </Text>
-        </View>
+        <TouchableOpacity 
+            style={styles.item}
+            onPress={() =>
+                navigation.navigate('Profile', { 
+                    title,
+                    content,
+                    image
+                }   
+            )}>
+        <Image 
+        source={{ uri: image}}
+        style={styles.image}
+        resizeMode='cover'
+        />
+        <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
     );
 };
 
 const styles = StyleSheet.create({
     item: {
-        backgroundColor: '#f9c2ff',
+        backgroundColor: '#ffffff',
         paddingBottom: 20,
-        paddingTop: 20,
         marginVertical: 8,
-        marginHorizontal: 16,
+        marginHorizontal: 10,
         flex: 1,
-        alignItems: 'center'
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: "#caccac",
     },
     title: {
         fontSize: 32,
         textAlign: 'center'
     },
     image:{
-        width: imageWidth,
-        height: imageHeight,
+        width: imageWidth - 22,
+        height: 150,
     }
 });
 
